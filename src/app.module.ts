fb0d43f9b27dd17eba20f444cc7d0cdec92de69b
majdshatti <majdshatti8@gmail.com>
2022-09-17 21:57:30 +0300

    Typeorm dataSource added to imports

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,10 +2,13 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { AlbumModule } from './album/album.module';
+import { typeOrmConfig } from './config/typeorm.config';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [AlbumModule],
+  imports: [TypeOrmModule.forRoot(typeOrmConfig), AlbumModule],
   controllers: [AppController],
   providers: [AppService],
 })
+
 export class AppModule {}
