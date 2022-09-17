8f47270b54f990eabc37b9f02b520f50d5b9dfd9
majdshatti <majdshatti8@gmail.com>
2022-09-17 21:50:03 +0300

    Controller functions improved

--- src/album/album.controller.ts
+++ src/album/album.controller.ts
@@ -1,19 +1,40 @@
-import { Body, Controller, Get, Post, UsePipes, ValidationPipe } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Get,
+  Param,
+  Post,
+  UsePipes,
+  ParseIntPipe,
+  ValidationPipe,
+  Patch,
+} from '@nestjs/common';
+
 import { AlbumService } from './album.service';
 import { CreateAlbumDto } from './dto/create-album.dto';
+import { UpdateAlbumDto } from './dto/update-album.dto';
+import { AlbumEntity } from './album.entity';
 
 @Controller('album')
 export class AlbumController {
   constructor(private albumService: AlbumService) {}
 
-  @Get()
-  getAllAlbums() {
-    return this.albumService.getAllAlbums();
+  @Get('/:id')
+  getAlbumById(@Param('id', ParseIntPipe) id: number): Promise<AlbumEntity> {
+    return this.albumService.getAlbumById(id);
   }
 
   @Post()
   @UsePipes(ValidationPipe)
-  createAlbum(@Body() createAlbumDto: CreateAlbumDto) {
-    return this.albumService.createAlbum(createAlbumDto);
+  createAlbum(@Body() body: CreateAlbumDto): Promise<AlbumEntity> {
+    return this.albumService.createAlbum(body);
+  }
+
+  @Patch('/:id')
+  updateAlbum(
+    @Param('id', ParseIntPipe) id: number,
+    @Body() body: UpdateAlbumDto,
+  ): Promise<AlbumEntity> {
+    return this.albumService.updateAlbum(id, body);
   }
 }
