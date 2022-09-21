a0be293eac1eeb6228f188364696612444f81b38
majdshatti <majdshatti8@gmail.com>
2022-09-21 22:48:04 +0300

    feat: createUser and getUserByUsername are added

---
+++ src/feature/user/user.repository.ts
@@ -0,0 +1,34 @@
+// Utilities
+import * as bcrypt from 'bcrypt';
+// Database App dataSource
+import { AppDataSource } from 'src/database/dataSource';
+// Data Transfer Object
+import { RegisterUserDto } from '../auth/dto';
+// Entity
+import { User } from './user.entity';
+
+export const UserRepositroy = AppDataSource.manager.getRepository(User).extend({
+  /**
+   * Create user by register dto
+   *
+   * @param registerUserDto Register data transfer object
+   * @returns Promise<User>
+   */
+  async createUser(registerUserDto: RegisterUserDto): Promise<User> {
+    const user = new User();
+
+    // Salt value
+    const salt = await bcrypt.genSalt(10);
+
+    user.username = registerUserDto.username;
+    // Hash password with the salt value
+    user.password = await bcrypt.hash(registerUserDto.password, salt);
+    user.email = registerUserDto.email;
+
+    return user.save();
+  },
+
+  async getUserByUsername(username: string): Promise<User> {
+    return await this.findOneBy({ username });
+  },
+});
