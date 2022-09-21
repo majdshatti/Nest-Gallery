45bd8f57786a1577af33957f5278969d0c1b4851
majdshatti <majdshatti8@gmail.com>
2022-09-21 22:29:02 +0300

    feat: register and login functionality added

--- src/feature/auth/auth.controller.ts
+++ src/feature/auth/auth.controller.ts
@@ -1,4 +1,22 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Post, Body } from '@nestjs/common';
+// Entity
+import { User } from '../user';
+// Service
+import { AuthService } from './auth.service';
+// Data Transfer Objects
+import { LoginUserDto, RegisterUserDto } from './dto/';
 
 @Controller('auth')
-export class AuthController {}
+export class AuthController {
+  constructor(private authService: AuthService) {}
+
+  @Post('/register')
+  async registerUser(@Body() registerUserDto: RegisterUserDto): Promise<User> {
+    return await this.authService.registerUser(registerUserDto);
+  }
+
+  @Post('/login')
+  async loginUser(@Body() loginUserDto: LoginUserDto) {
+    return await this.authService.loginUser(loginUserDto);
+  }
+}
