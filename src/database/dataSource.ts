00741ec4afbffea4664387303916fb0bc6256a39
majdshatti <majdshatti8@gmail.com>
2022-09-21 21:36:59 +0300

    initial: dataSource initialized

---
+++ src/database/dataSource.ts
@@ -0,0 +1,12 @@
+import { DataSource } from 'typeorm';
+import { dataSourceOptions } from './dataSourceOptions';
+
+export const AppDataSource = new DataSource(dataSourceOptions);
+
+AppDataSource.initialize()
+  .then(() => {
+    console.log('Data Source has been initialized!');
+  })
+  .catch((err) => {
+    console.error('Error during Data Source initialization', err);
+  });
