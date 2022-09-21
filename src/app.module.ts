3f01838cc8718eb23601a565ff5540fc872e142c
majdshatti <majdshatti8@gmail.com>
2022-09-21 22:41:27 +0300

    refactor: Datasource and configs refactored

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,14 +1,26 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { AlbumModule } from './album/album.module';
-import { typeOrmConfig } from './config/typeorm.config';
+import { AlbumModule } from './feature/album/album.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { AuthModule } from './feature/auth/auth.module';
+import { AppConfigModule } from './config/app/config.module';
+import { DbConfigModule } from './config/database/config.module';
+import { dataSourceOptions } from './database/dataSourceOptions';
+import { UserModule } from './feature/user/user.module';
 
 @Module({
-  imports: [TypeOrmModule.forRoot(typeOrmConfig), AlbumModule],
+  imports: [
+    AppConfigModule,
+    DbConfigModule,
+    TypeOrmModule.forRoot(dataSourceOptions),
+
+    AlbumModule,
+    AuthModule,
+    UserModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
-
 export class AppModule {}
