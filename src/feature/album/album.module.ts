12fa97e1b2f2cdd36f41b8f254f30cd086241ec5
majdshatti <majdshatti8@gmail.com>
2022-09-21 21:45:11 +0300

    feat: AuthModule added to the module

--- src/feature/album/album.module.ts
+++ src/feature/album/album.module.ts
@@ -1,8 +1,10 @@
 import { Module } from '@nestjs/common';
+import { AuthModule } from '../auth/auth.module';
 import { AlbumController } from './album.controller';
 import { AlbumService } from './album.service';
 
 @Module({
+  imports: [AuthModule],
   controllers: [AlbumController],
   providers: [AlbumService],
 })
