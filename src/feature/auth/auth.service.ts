d2a52ec1a54645a006fdb4673cdf9a8a01127216
majdshatti <majdshatti8@gmail.com>
2022-09-21 22:31:25 +0300

    feat: Login and Register services added

--- src/feature/auth/auth.service.ts
+++ src/feature/auth/auth.service.ts
@@ -1,4 +1,40 @@
-import { Injectable } from '@nestjs/common';
+// Nest
+import {
+  Injectable,
+  UnauthorizedException,
+  NotFoundException,
+} from '@nestjs/common';
+// Data Transfer Objects
+import { LoginUserDto, RegisterUserDto } from './dto';
+// Repository
+import { User, UserRepositroy } from '../user';
+// JWT Service
+import { JwtService } from '@nestjs/jwt';
+import { IPayload } from './interfaces/payload.interface';
 
 @Injectable()
-export class AuthService {}
+export class AuthService {
+  constructor(private jwtService: JwtService) {}
+
+  async registerUser(registerUserDto: RegisterUserDto): Promise<User> {
+    return await UserRepositroy.createUser(registerUserDto);
+  }
+
+  async loginUser(
+    loginUserDto: LoginUserDto,
+  ): Promise<{ accessToken: string }> {
+    const { username, password } = loginUserDto;
+    const user: User = await UserRepositroy.getUserByUsername(username);
+
+    if (!user) throw new NotFoundException('Invalied Credentials');
+
+    const isMatched: boolean = await user.matchPassword(password);
+
+    if (!isMatched) throw new UnauthorizedException('Invalied Credentials');
+
+    const payload: IPayload = { username };
+    const accessToken: string = await this.jwtService.sign(payload);
+
+    return { accessToken };
+  }
+}
