21c081b37d736bda4b0aaa6cf5d055f3a55a292e
majdshatti <majdshatti8@gmail.com>
2022-09-21 22:33:01 +0300

    Passport strategy (jwt) for validating jwt token is added

---
+++ src/feature/auth/jwt.strategy.ts
@@ -0,0 +1,28 @@
+import { ExtractJwt, Strategy } from 'passport-jwt';
+import { PassportStrategy } from '@nestjs/passport';
+import { Injectable, UnauthorizedException } from '@nestjs/common';
+import { IPayload } from './interfaces/payload.interface';
+import { AppDataSource } from 'src/database/dataSource';
+import { User } from '../user';
+
+@Injectable()
+export class JwtStrategy extends PassportStrategy(Strategy) {
+  constructor() {
+    super({
+      jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
+      ignoreExpiration: false,
+      secretOrKey: 'dabestsecretever',
+    });
+  }
+
+  async validate(payload: IPayload): Promise<User> {
+    const { username } = payload;
+    const user = await AppDataSource.getRepository(User).findOneBy({
+      username,
+    });
+
+    if (!user) throw new UnauthorizedException('Invalied token');
+
+    return user;
+  }
+}
