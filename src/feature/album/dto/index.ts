76a623a2b4549a09062cf9964b277dd002403a52
majdshatti <majdshatti8@gmail.com>
2022-09-21 21:41:55 +0300

    feat: Index for exporting dtos added

---
+++ src/feature/album/dto/index.ts
@@ -0,0 +1,5 @@
+import { CreateAlbumDto } from './create-album.dto';
+import { UpdateAlbumDto } from './update-album.dto';
+import { FilterAlbumDto } from './filter-album.dto';
+
+export { CreateAlbumDto, UpdateAlbumDto, FilterAlbumDto };
