d31e0223e3833e431e9c3b6f34bb0587ae61dac8
majdshatti <majdshatti8@gmail.com>
2022-09-23 23:44:26 +0300

    fix: removed un necessary lines that causes errors

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,13 +1,14 @@
+// Nest
 import { Module } from '@nestjs/common';
-import { ConfigModule } from '@nestjs/config';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
-import { AlbumModule } from './feature/album/album.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { AuthModule } from './feature/auth/auth.module';
+// DataSoruce
+import { dataSourceOptions } from './database/dataSourceOptions';
+// Configs
 import { AppConfigModule } from './config/app/config.module';
 import { DbConfigModule } from './config/database/config.module';
-import { dataSourceOptions } from './database/dataSourceOptions';
+// Feature modules
+import { AlbumModule } from './feature/album/album.module';
+import { AuthModule } from './feature/auth/auth.module';
 import { UserModule } from './feature/user/user.module';
 
 @Module({
@@ -15,12 +16,9 @@ import { UserModule } from './feature/user/user.module';
     AppConfigModule,
     DbConfigModule,
     TypeOrmModule.forRoot(dataSourceOptions),
-
     AlbumModule,
     AuthModule,
     UserModule,
   ],
-  controllers: [AppController],
-  providers: [AppService],
 })
 export class AppModule {}
