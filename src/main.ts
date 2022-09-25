56fd5fd3b2c3b763a6e3616e22275a59e511b9cb
majdshatti <majdshatti8@gmail.com>
2022-09-25 19:15:12 +0300

    feat: add global serialization interceptor

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,11 @@
-import { NestFactory } from '@nestjs/core';
+import { ClassSerializerInterceptor } from '@nestjs/common';
+import { NestFactory, Reflector } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { AppConfigService } from './config/app/config.service';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.useGlobalInterceptors(new ClassSerializerInterceptor(app.get(Reflector)));
   // get app configs
   const appConfig: AppConfigService = app.get(AppConfigService);
 
