c364f3b2f8e2e2c711dc3ecde9f08c36ef3cc98f
majdshatti <majdshatti8@gmail.com>
2022-09-25 21:54:22 +0300

    feat: add uuid for creating user

--- src/feature/user/user.repository.ts
+++ src/feature/user/user.repository.ts
@@ -1,11 +1,12 @@
-// Utilities
-import * as bcrypt from 'bcrypt';
 // Database App dataSource
 import { AppDataSource } from 'src/database/dataSource';
 // Data Transfer Object
 import { RegisterUserDto } from '../auth/dto';
 // Entity
 import { User } from './user.entity';
+// Utilities
+import * as bcrypt from 'bcrypt';
+import { v4 as uuid } from 'uuid';
 
 export const UserRepositroy = AppDataSource.manager.getRepository(User).extend({
   /**
@@ -20,14 +21,13 @@ export const UserRepositroy = AppDataSource.manager.getRepository(User).extend({
     // Salt value
     const salt = await bcrypt.genSalt(10);
 
+    user._id = uuid();
     user.username = registerUserDto.username;
     // Hash password with the salt value
     user.password = await bcrypt.hash(registerUserDto.password, salt);
     user.email = registerUserDto.email;
 
     await user.save();
-
-    delete user.password;
 
     return user;
   },
@@ -39,9 +39,6 @@ export const UserRepositroy = AppDataSource.manager.getRepository(User).extend({
    * @returns Promise<User>
    */
   async getUserByUsername(username: string): Promise<User> {
-    return await this.createQueryBuilder('user')
-      .select('user.username', username)
-      .addSelect('user.password')
-      .getOne();
+    return await this.findOneBy({ username });
   },
 });
