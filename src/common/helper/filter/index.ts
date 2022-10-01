bc7701c885ea899f450c56ba3985b43733943fcc
majdshatti <majdshatti8@gmail.com>
2022-10-01 23:47:24 +0300

    feat: exporting FilterOperator and getOperatorValue

--- src/common/helper/filter/index.ts
+++ src/common/helper/filter/index.ts
@@ -1,5 +1,7 @@
 import { FilterDto } from './dto/filter.dto';
 import { IFilterOptions } from './interfaces/options.interface';
 import { filter } from './filter';
+import { FilterOperator } from './enum/filter-operations.enum';
+import { getOperatorValue } from './helper/getOperatorValue.helper';
 
-export { filter, FilterDto, IFilterOptions };
+export { filter, FilterDto, IFilterOptions, FilterOperator, getOperatorValue };
