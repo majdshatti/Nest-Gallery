1f4f288e3a63da26f89281a54d99410609e60284
majdshatti <majdshatti8@gmail.com>
2022-10-01 23:49:01 +0300

    fix: fixing filterableColumns type

--- src/common/helper/filter/interfaces/options.interface.ts
+++ src/common/helper/filter/interfaces/options.interface.ts
@@ -1,6 +1,12 @@
+import { FilterOperator } from '../';
+
+interface IFilterableColumnsObject {
+  [key: string]: FilterOperator;
+}
+
 export interface IFilterOptions {
   sortableColumns?: string[];
   searchableColumns?: string[];
   defaultSortBy?: [[string, 'DESC' | 'ASC']];
-  filterableColums?: {};
+  filterableColumns?: IFilterableColumnsObject;
 }
