b88688b837b9c7ac3f5c5f112b257e0ae880a582
majdshatti <majdshatti8@gmail.com>
2022-11-19 19:54:34 +0200

    feat: apply exception factory to the validation pipe

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,24 @@
-import { ClassSerializerInterceptor } from '@nestjs/common';
+import { ClassSerializerInterceptor, ValidationPipe } from '@nestjs/common';
 import { NestFactory, Reflector } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { exceptionFactory } from './common/utils';
+import { AppExceptionHandler } from './common/exceptions';
 import { AppConfigService } from './config/app/config.service';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  app.useGlobalPipes(
+    new ValidationPipe({
+      exceptionFactory,
+    }),
+  );
+
   app.useGlobalInterceptors(new ClassSerializerInterceptor(app.get(Reflector)));
+
+  // const { httpAdapter } = app.get(HttpAdapterHost);
+  app.useGlobalFilters(new AppExceptionHandler());
+
   // get app configs
   const appConfig: AppConfigService = app.get(AppConfigService);
 
