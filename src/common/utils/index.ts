b3e60007b2b451a4095a99230928df681b5257dc
majdshatti <majdshatti8@gmail.com>
2022-11-19 19:55:59 +0200

    feat: export more modules

--- src/common/utils/index.ts
+++ src/common/utils/index.ts
@@ -1,4 +1,6 @@
 import { arrayify } from './dataStructures/arrayify.util';
 import { isObjKey } from './dataStructures/isObjectKey.util';
+import { sanitizeString } from './strings/sanitize-string.util';
+import { exceptionFactory } from './exceptions/validation-factory.util';
 
-export { arrayify, isObjKey };
+export { arrayify, isObjKey, sanitizeString, exceptionFactory };
--- src/feature/album/index.ts
+++ src/feature/album/index.ts
@@ -1,3 +1,15 @@
+import { AlbumController } from './album.controller';
 import { Album } from './album.entity';
+import { AlbumModule } from './album.module';
+import { AlbumRepository } from './album.repository';
+import { AlbumService } from './album.service';
+import { FilterAlbumDto } from './dto/filter-album.dto';
 
-export { Album };
+export {
+  Album,
+  FilterAlbumDto,
+  AlbumService,
+  AlbumRepository,
+  AlbumModule,
+  AlbumController,
+};
