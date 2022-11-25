d0052435d0f89d4e4a80012c3e554a73e1a3e8f3
majdshatti <majdshatti8@gmail.com>
2022-11-25 19:42:14 +0200

    style: removed unnecessary comment

--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,14 @@
-import { ClassSerializerInterceptor, ValidationPipe } from '@nestjs/common';
 import { NestFactory, Reflector } from '@nestjs/core';
+import { ClassSerializerInterceptor, ValidationPipe } from '@nestjs/common';
 import { AppModule } from './app.module';
-import { exceptionFactory } from './common/utils';
 import { AppExceptionHandler } from './common/exceptions';
 import { AppConfigService } from './config/app/config.service';
+import { exceptionFactory } from './common/utils';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
+  // Providing custom form of validation errors
   app.useGlobalPipes(
     new ValidationPipe({
       exceptionFactory,
@@ -15,8 +16,6 @@ async function bootstrap() {
   );
 
   app.useGlobalInterceptors(new ClassSerializerInterceptor(app.get(Reflector)));
-
-  // const { httpAdapter } = app.get(HttpAdapterHost);
   app.useGlobalFilters(new AppExceptionHandler());
 
   // get app configs
