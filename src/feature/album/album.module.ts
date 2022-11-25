b4dc9c8835bb8bbb73829686e446c0728df10eed
majdshatti <majdshatti8@gmail.com>
2022-11-25 19:53:04 +0200

    refactor: add album repository

--- src/feature/album/album.module.ts
+++ src/feature/album/album.module.ts
@@ -1,11 +1,14 @@
-import { Module } from '@nestjs/common';
+import { Module, Logger } from '@nestjs/common';
 import { AuthModule } from '../auth/auth.module';
 import { AlbumController } from './album.controller';
 import { AlbumService } from './album.service';
+import { Album } from './album.entity';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { AlbumRepository } from './album.repository';
 
 @Module({
-  imports: [AuthModule],
+  imports: [AuthModule, TypeOrmModule.forFeature([Album])],
   controllers: [AlbumController],
-  providers: [AlbumService],
+  providers: [AlbumService, AlbumRepository],
 })
 export class AlbumModule {}
