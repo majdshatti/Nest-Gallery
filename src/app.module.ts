7d8eb1eca5059378995bed5f335e160c32c05a99
majdshatti <majdshatti8@gmail.com>
2022-11-25 19:40:36 +0200

    feat: add middleware for logging http requests

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,24 +1,36 @@
 // Nest
-import { Module } from '@nestjs/common';
+import { Module, MiddlewareConsumer, NestModule } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 // DataSoruce
 import { dataSourceOptions } from './database/dataSourceOptions';
 // Configs
 import { AppConfigModule } from './config/app/config.module';
 import { DbConfigModule } from './config/database/config.module';
+// Multer
+import { MulterModule } from '@nestjs/platform-express';
+import { memoryStorage } from 'multer';
 // Feature modules
-import { AlbumModule } from './feature/album/album.module';
+import { AlbumModule } from './feature/album';
 import { AuthModule } from './feature/auth/auth.module';
 import { UserModule } from './feature/user/user.module';
+import { HttpLoggerMiddleware } from './common/middlewares';
 
 @Module({
   imports: [
     AppConfigModule,
     DbConfigModule,
     TypeOrmModule.forRoot(dataSourceOptions),
+    MulterModule.register({
+      storage: memoryStorage(),
+    }),
     AlbumModule,
     AuthModule,
     UserModule,
   ],
+  providers: [],
 })
-export class AppModule {}
+export class AppModule implements NestModule {
+  configure(consumer: MiddlewareConsumer) {
+    consumer.apply(HttpLoggerMiddleware).forRoutes('*');
+  }
+}
