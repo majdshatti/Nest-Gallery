20ac5fa3606edd695ec53f9d0f278aaeb1a0cbc9
majdshatti <majdshatti8@gmail.com>
2022-12-20 21:43:22 +0200

    refactor: logic in repository is moved to service

--- src/feature/album/album.repository.ts
+++ src/feature/album/album.repository.ts
@@ -2,57 +2,15 @@
 import { User } from '../user';
 import { Album } from './album.entity';
 // Data transfer objects
-import { CreateAlbumDto, UpdateAlbumDto, FilterAlbumDto } from './dto/';
+import { UpdateAlbumDto } from './dto/';
 // Utilites
-import { v4 as uuid } from 'uuid';
-import { S3Service } from 'src/common/services/aws/s3/s3.service';
-import { Injectable, InternalServerErrorException } from '@nestjs/common';
+import { Injectable } from '@nestjs/common';
 import { Repository, DataSource } from 'typeorm';
 
 @Injectable()
 export class AlbumRepository extends Repository<Album> {
   constructor(private dataSource: DataSource) {
     super(Album, dataSource.createEntityManager());
-  }
-  /**
-   * Get albums with the ability of providing querys
-   *
-   * @param filterAlbumDto
-   * @returns Album[]: Array of albums
-   */
-  async getAlbums(
-    filterAlbumDto: FilterAlbumDto,
-    user: User,
-  ): Promise<Album[]> {
-    const { name, search, type } = filterAlbumDto;
-
-    // Init query builder
-    const queryBuilder = this.createQueryBuilder('album');
-
-    // Get all public albums except user's album
-    if (type === 'public') {
-      queryBuilder.andWhere(
-        `album.isPrivate = :isPrivate AND album.userId != :userId`,
-        { isPrivate: 0, userId: user.id },
-      );
-    }
-    // Or get user's albums
-    else if (!type || type === 'user') {
-      queryBuilder.andWhere(`album.userId = :userId`, { userId: user.id });
-    }
-
-    // Check if name or description is passed as a query
-    if (name)
-      queryBuilder.andWhere(`album.name LIKE :name`, { name: `%${name}%` });
-
-    if (search)
-      queryBuilder.andWhere(
-        `album.name LIKE :search OR album.description LIKE :search`,
-        { search: `%${search}%` },
-      );
-
-    // Return all filtered albums
-    return queryBuilder.getMany();
   }
 
   /**
@@ -61,35 +19,8 @@ export class AlbumRepository extends Repository<Album> {
    * @param body
    * @returns Album: Stored album
    */
-  async createAlbum(body: CreateAlbumDto, user: User): Promise<Album> {
-    const { name, description, isPrivate, image } = body;
-    const s3Service = new S3Service();
-    const album = new Album();
-
-    album._id = uuid();
-    album.name = name;
-    album.description = description;
-    album.isPrivate = parseInt(isPrivate, 10) == 1 ? true : false;
-    album.user = user;
-
-    try {
-      const uploadedFile = await s3Service.uploadFile(
-        image,
-        `users/${user.username}`,
-      );
-      album.image = uploadedFile.Location;
-    } catch (e) {
-      throw new InternalServerErrorException(
-        e.message,
-        'Internal Server Error',
-      );
-    }
-
-    await album.save();
-
-    delete album.user;
-
-    return album;
+  async createAlbum(album: Album): Promise<Album> {
+    return await album.save();
   }
 
   /**
@@ -100,12 +31,6 @@ export class AlbumRepository extends Repository<Album> {
    * @returns Updated album
    */
   async updateAlbum(album: Album, body: UpdateAlbumDto): Promise<Album> {
-    const { name, description, isPrivate, image } = body;
-    album.name = name ?? album.name;
-    album.description = description ?? album.description;
-    album.isPrivate = isPrivate ?? album.isPrivate;
-    album.image = image ?? album.image;
-
     return await album.save();
   }
 }
